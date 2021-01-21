a74531fa34c2577d9361781b5a31873cb702ac3d
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2021-01-20 21:30:38 -0500

    feat: now supports typescript, still testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
 const path = require("path");
 
+const includePaths = [path.resolve(__dirname, "./src")];
+
 module.exports = {
   devServer: {
-    contentBase: path.resolve(__dirname, "./src"),
+    contentBase: path.join(__dirname, "./dist"),
     historyApiFallback: true,
+    hot: true,
+    port: 3000,
+    watchContentBase: true,
   },
-  entry: path.resolve(__dirname, "./src/index.js"),
+  devtool: "source-map",
+  entry: path.resolve(__dirname, "./src/index.tsx"),
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js?$/,
+        exclude: /node-modules/,
+        include: includePaths,
         use: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node-modules/,
+        include: includePaths,
+        use: "ts-loader",
       },
     ],
   },
   output: {
     filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", "jsx"],
   },
 };
